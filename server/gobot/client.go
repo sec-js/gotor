d983dfdf4bc0daddf252720201f9ff519de14e75
Akeem King <akeemtlking@gmail.com>
2020-06-08 21:27:36 -0400

    Renaming files

---
+++ server/gobot/client.go
@@ -0,0 +1,67 @@
+package gobot
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	urllib "net/url"
+	"strings"
+	"time"
+)
+
+// Client represents a GoBot HTTP client
+type Client interface {
+	Get(string) (*http.Response, error)
+	Head(string) (*http.Response, error)
+}
+
+// ClientConfig contains configuration for a client
+type ClientConfig struct {
+	addr    string
+	port    string
+	timeout int
+}
+
+type dualClient struct {
+	regClient *http.Client
+	torClient *http.Client
+}
+
+func (d *dualClient) Head(url string) (*http.Response, error) {
+	if strings.Contains(url, ".onion") {
+		return d.torClient.Head(url)
+	}
+
+	return d.regClient.Head(url)
+}
+
+func (d *dualClient) Get(url string) (*http.Response, error) {
+	if strings.Contains(url, ".onion") {
+		return d.torClient.Get(url)
+	}
+
+	return d.regClient.Get(url)
+}
+
+// NewTorClient creates an HTTP client capable of performing TOR requests.
+func newTorClient(config *ClientConfig) *dualClient {
+	if config.addr == "" {
+		config.addr = defaultHost
+	}
+
+	if config.port == "" {
+		config.port = defaultPort
+	}
+
+	torProxyURL, err := urllib.Parse(fmt.Sprintf("socks5://%s:%s", config.addr, config.port))
+	if err != nil {
+		log.Fatal("Unable to parse Tor Proxy URL. Error:", err)
+	}
+	torTransport := &http.Transport{Proxy: http.ProxyURL(torProxyURL)}
+
+	// Creating both clients for regular browsing and Tor
+	torc := http.Client{Transport: torTransport, Timeout: time.Second * time.Duration(config.timeout)}
+	regc := http.Client{Timeout: time.Second * time.Duration(config.timeout)}
+	return &dualClient{regClient: &regc, torClient: &torc}
+
+}
--- server/gobot/parse.go
+++ server/gobot/parse.go
@@ -2,34 +2,11 @@ package gobot
 
 import (
 	"errors"
-	"fmt"
-	"log"
-	"net/http"
 	urllib "net/url"
-	"strings"
 	"sync"
-	"time"
 
 	"golang.org/x/net/html"
 )
-
-// Client represents a GoBot HTTP client
-type Client interface {
-	Get(string) (*http.Response, error)
-	Head(string) (*http.Response, error)
-}
-
-// ClientConfig contains configuration for a client
-type ClientConfig struct {
-	addr    string
-	port    string
-	timeout int
-}
-
-type dualClient struct {
-	regClient *http.Client
-	torClient *http.Client
-}
 
 const (
 	defaultHost    = "127.0.0.1"
@@ -37,47 +14,8 @@ const (
 	defaultTimeout = 60
 )
 
-// NewTorClient creates an HTTP client capable of performing TOR requests.
-func newTorClient(config *ClientConfig) *dualClient {
-	if config.addr == "" {
-		config.addr = defaultHost
-	}
-
-	if config.port == "" {
-		config.port = defaultPort
-	}
-
-	torProxyURL, err := urllib.Parse(fmt.Sprintf("socks5://%s:%s", config.addr, config.port))
-	if err != nil {
-		log.Fatal("Unable to parse Tor Proxy URL. Error:", err)
-	}
-	torTransport := &http.Transport{Proxy: http.ProxyURL(torProxyURL)}
-
-	// Creating both clients for regular browsing and Tor
-	torc := http.Client{Transport: torTransport, Timeout: time.Second * time.Duration(config.timeout)}
-	regc := http.Client{Timeout: time.Second * time.Duration(config.timeout)}
-	return &dualClient{regClient: &regc, torClient: &torc}
-
-}
-
-func (d *dualClient) Head(url string) (*http.Response, error) {
-	if strings.Contains(url, ".onion") {
-		return d.torClient.Head(url)
-	}
-
-	return d.regClient.Head(url)
-}
-
-func (d *dualClient) Get(url string) (*http.Response, error) {
-	if strings.Contains(url, ".onion") {
-		return d.torClient.Get(url)
-	}
-
-	return d.regClient.Get(url)
-}
-
 // Parses value to retrieve href
-func findHrefs(attributes []html.Attribute) []string {
+func parseHrefs(attributes []html.Attribute) []string {
 	foundUrls := make([]string, 0)
 	for i := 0; i < len(attributes); i++ {
 		if attributes[i].Key == "href" {
@@ -115,7 +53,7 @@ func GetLinks(searchURL string) (map[string]bool, error) {
 			token := tokenizer.Token()
 			// If link tag is found, append it to slice
 			if token.Data == "a" {
-				urlsFound := findHrefs(token.Attr)
+				urlsFound := parseHrefs(token.Attr)
 				totalUrls = append(totalUrls, urlsFound...)
 			}
 		}
