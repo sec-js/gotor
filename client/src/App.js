1100a118e13722f128c0fe21f0125ad25cf0fd57
Akeem King <akeemtlking@gmail.com>
2020-06-08 22:21:34 -0400

    Adding list of urls

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
-import { TextField, Select, InputLabel, MenuItem, Button } from '@material-ui/core';
+import { TextField, Select, InputLabel, MenuItem, Button, ListItem, ListItemText } from '@material-ui/core';
+import { FixedSizeList } from 'react-window';
 import { styled } from '@material-ui/styles';
 import axios from 'axios';
 import './App.css';
@@ -35,6 +36,34 @@ class Selector extends Component {
     }
 }
 
+class ResultList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: props.items
+        }
+        this.renderRow = this.renderRow.bind(this);
+    }
+
+    renderRow({ index, style }) {
+        return (
+            <ListItem button style={style} key={index}>
+                <ListItemText primary={this.state.items[index]}/>
+            </ListItem>
+        );
+    }
+
+    render() {
+        return (
+            <div>
+                <FixedSizeList height={400} width={300} itemSize={50} itemCount={this.state.items.length}>
+                    {this.renderRow}
+                </FixedSizeList>
+            </div>
+        )
+    }
+}
+
 const MainTextField = styled(TextField)({
     'padding-bottom': '10%'
 });
@@ -49,6 +78,7 @@ class App extends Component {
         super(props);
         this.state = {
             url: '',
+            links: {},
             selected: 'Get Links', 
             options: ['Get Links', 'Analyze']
         };
@@ -56,12 +86,11 @@ class App extends Component {
         this.handleTextChange = this.handleTextChange.bind(this);
         this.handleOptionChange = this.handleOptionChange.bind(this);
     }
-
     handleSubmit(event) {
         switch (this.state.selected) {
             case 'Get Links':
                 getLinks(this.state.url)
-                    .then(response => console.log(response.data))
+                    .then(({ data: links }) => this.setState({links}))
                     .catch(err => console.error(err));
                 break;
             case 'Analyze':
@@ -83,6 +112,18 @@ class App extends Component {
     }
 
     render() {
+        const urls = Object.keys(this.state.links);
+        if (urls.length) {
+            return (
+                <div style={{
+                    position: 'absolute', left: '50%', top: '50%',
+                    transform: 'translate(-50%, -50%)'
+                }} className="App">
+                    <ResultList items={urls}></ResultList>
+                </div>
+            )
+        }
+
         return (
             <div style={{
                 position: 'absolute', left: '50%', top: '50%',
