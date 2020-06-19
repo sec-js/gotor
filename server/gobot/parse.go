85818b51f9376c749164eef2007a423c281a0840
Akeem King <akeemtlking@gmail.com>
2020-06-19 17:45:41 -0400

    Officially use link as data type name

--- server/gobot/parse.go
+++ server/gobot/parse.go
@@ -2,37 +2,56 @@ package gobot
 
 import (
 	"errors"
+	"net/http"
 	"net/url"
 	"sync"
 
 	"golang.org/x/net/html"
 )
 
-// Link ...
-type Link struct {
+// LinkData ...
+type LinkData struct {
 	Name   string
 	Status bool
 }
 
 // Parses value to retrieve href
 func parseHrefs(attributes []html.Attribute) []string {
-	foundUrls := make([]string, 0)
+	links := make([]string, 0)
 	for i := 0; i < len(attributes); i++ {
 		if attributes[i].Key == "href" {
-			u, err := url.ParseRequestURI(attributes[i].Val)
-			if u == nil || err != nil {
+			link, err := url.ParseRequestURI(attributes[i].Val)
+			if link == nil || err != nil {
 				continue
 			}
-			if u.Scheme != "" {
-				foundUrls = append(foundUrls, u.String())
+			if link.Scheme != "" {
+				links = append(links, link.String())
 			}
 		}
 	}
-	return foundUrls
+	return links
+}
+
+type processFunction func(*http.Response, error)
+
+func processLinks(c Client, links []string, process processFunction) {
+	var wg sync.WaitGroup
+	var mux sync.RWMutex
+	for _, link := range links {
+		wg.Add(1)
+		go func(l string) {
+			defer wg.Done()
+			resp, err := c.Head(l)
+			mux.Lock()
+			process(resp, err)
+			mux.Unlock()
+		}(link)
+	}
+	wg.Wait()
 }
 
 // GetLinks returns a map that contains the links as keys and their statuses as values
-func GetLinks(searchURL string) ([]Link, error) {
+func GetLinks(searchURL string) ([]LinkData, error) {
 	// Creating new Tor connection
 	client := newDualClient(&ClientConfig{timeout: defaultTimeout})
 	resp, err := client.Get(searchURL)
@@ -43,7 +62,7 @@ func GetLinks(searchURL string) ([]Link, error) {
 
 	// Begin parsing HTML
 	tokenizer := html.NewTokenizer(resp.Body)
-	totalUrls := make([]string, 0)
+	links := make([]string, 0)
 	for notEnd := true; notEnd; {
 		currentTokenType := tokenizer.Next()
 		switch {
@@ -53,35 +72,22 @@ func GetLinks(searchURL string) ([]Link, error) {
 			token := tokenizer.Token()
 			// If link tag is found, append it to slice
 			if token.Data == "a" {
-				urlsFound := parseHrefs(token.Attr)
-				totalUrls = append(totalUrls, urlsFound...)
+				linksFound := parseHrefs(token.Attr)
+				links = append(links, linksFound...)
 			}
 		}
 	}
 
-	if len(totalUrls) == 0 {
+	if len(links) == 0 {
 		return nil, errors.New("no links found for URL")
 	}
 
-	links := make([]Link, 0)
-	var wg sync.WaitGroup
-	var mux sync.RWMutex
-	var link Link
-	for _, u := range totalUrls {
-		wg.Add(1)
-		go func(u string) {
-			defer wg.Done()
-			resp, err := client.Head(u)
-			mux.Lock()
-			link = Link{
-				Name:   resp.Request.URL.String(),
-				Status: err == nil && resp.StatusCode < 400,
-			}
-			links = append(links, link)
-			mux.Unlock()
-		}(u)
-	}
-	wg.Wait()
-
-	return links, nil
+	linkDataList := make([]LinkData, 0)
+	processLinks(client, links, func(r *http.Response, e error) {
+		linkDataList = append(linkDataList, LinkData{
+			Name:   r.Request.URL.String(),
+			Status: e == nil && r.StatusCode < 400,
+		})
+	})
+	return linkDataList, nil
 }
